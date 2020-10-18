7477eae5db452704f4ac635e3d4c46d348dca773
dknuth <dknuth0101@gmail.com>
2020-10-18 14:06:48 -0700

    adding rectangle namespace, adding pho instance constructor that uses the namespaces

--- geom/namespace.go
+++
@@ -1,9 +0,0 @@
-package geom
-
-import (
-	"github.com/gravestench/pho/geom/circle"
-)
-
-type GeomNamespace struct {
-	Circle circle.Namespace
-}
--- pho.go
+++ pho.go
@@ -1,7 +1,24 @@
 package pho
 
-import "github.com/gravestench/pho/geom"
+import (
+	"github.com/gravestench/pho/geom/circle"
+	"github.com/gravestench/pho/geom/rectangle"
+)
 
 type Pho struct {
-	Geom geom.Namespace
+	*Geom
+}
+
+type Geom struct {
+	Circle *circle.Namespace
+	Rectangle *rectangle.Namespace
+}
+
+func New() *Pho {
+	return &Pho{
+		&Geom{
+			Circle: &circle.Namespace{},
+			Rectangle: &rectangle.Namespace{},
+		},
+	}
 }
